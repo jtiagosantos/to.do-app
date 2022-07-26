89760b53136bdf167284312918d23293ca1f53b8
jtiagosantos <tiagosan040@gmail.com>
2022-07-26 18:09:59 -0300

    complete challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,19 +5,43 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+import { generateId } from '../utils/generateId';
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const task = {
+      id: generateId(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((prevState) => ([
+      ...prevState,
+      task,
+    ]));
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === id) {
+        const updatedTask = {
+          ...task,
+          done: !task.done,
+        }
+
+        return updatedTask;
+      }
+
+      return task;
+    });
+
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter((task) => task.id !== id));
   }
 
   return (
